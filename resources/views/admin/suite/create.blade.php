4fea13b22cad27ccf3f8e55b2715d3dc59ef581a
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-09-03 12:49:14 +0200

    fix create, manca la logica per l'indirizzo(suggerimenti come detto da loris)

--- resources/views/admin/suite/create.blade.php
+++ resources/views/admin/suite/create.blade.php
@@ -9,8 +9,8 @@
     <form action="{{ route('admin.suite.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="w-50 m-5">
-            <label for="suite_title" class="form-label">Title:</label>
-            <input type="text" class="form-control" id="suite_title" placeholder="Suite Title" name="title">
+            <label for="suite_title" class="form-label">*Title:</label>
+            <input type="text" class="form-control" id="suite_title" placeholder="Suite Title" name="title" value="{{ old('title') }}" required >
         </div>
         @error('title')
             <span class="bg-danger" role="alert">
@@ -18,8 +18,8 @@
             </span>
         @enderror
         <div class="w-50 m-5">
-            <label for="suite_room" class="form-label">Rooms:</label>
-            <input type="number" class="form-control" id="suite_room" placeholder="Number of rooms" name="room" min="1" max="20">
+            <label for="suite_room" class="form-label">*Rooms:</label>
+            <input type="number" class="form-control" id="suite_room" placeholder="Number of rooms" name="room" min="1" max="20" value="{{ old('room') }}" required >
         </div>
         @error('room')
             <span class="bg-danger" role="alert">
@@ -27,8 +27,8 @@
             </span>
         @enderror
         <div class="w-50 m-5">
-            <label for="suite_bed" class="form-label">Beds:</label>
-            <input type="number" class="form-control" id="suite_bed" placeholder="Number of beds" name="bed" min="1" max="20">
+            <label for="suite_bed" class="form-label">*Beds:</label>
+            <input type="number" class="form-control" id="suite_bed" placeholder="Number of beds" name="bed" min="1" max="20" value="{{ old('bed') }}" required >
         </div>
         @error('bed')
             <span class="bg-danger" role="alert">
@@ -36,8 +36,8 @@
             </span>
         @enderror
         <div class="w-50 m-5">
-            <label for="suite_bathroom" class="form-label">Bathrooms:</label>
-            <input type="number" class="form-control" id="suite_bathroom" placeholder="Number of bathrooms" name="bathroom" min="1" max="10">
+            <label for="suite_bathroom" class="form-label">*Bathrooms:</label>
+            <input type="number" class="form-control" id="suite_bathroom" placeholder="Number of bathrooms" name="bathroom" min="1" max="10" value="{{ old('bathroom') }}" required >
         </div>
         @error('bathroom')
             <span class="bg-danger" role="alert">
@@ -45,8 +45,8 @@
             </span>
         @enderror
         <div class="w-50 m-5">
-            <label for="suite_squareM" class="form-label">Square Meters:</label>
-            <input type="number" class="form-control" id="suite_squareM" placeholder="Square meters" name="squareM" min="20">
+            <label for="suite_squareM" class="form-label">*Square Meters:</label>
+            <input type="number" class="form-control" id="suite_squareM" placeholder="Square meters" name="squareM" min="25" value="{{ old('squareM') }}" required >
         </div>
         @error('squareM')
             <span class="bg-danger" role="alert">
@@ -54,17 +54,17 @@
             </span>
         @enderror
         <div class="w-50 m-5">
-            <label for="suite_address" class="form-label">Address:</label>
-            <input type="text" class="form-control" id="suite_address" placeholder="Address" name="address">
+            <label for="suite_address" class="form-label">*Address:</label>
+            <input type="text" class="form-control" id="suite_address" placeholder="Address" name="address" value="{{ old('address') }}" required >
         </div>
         @error('address')
             <span class="bg-danger" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
         @enderror
-        <div class="w-50 m-5">
+        {{-- <div class="w-50 m-5">
             <label for="suite_civic" class="form-label">House Number:</label>
-            <input type="text" class="form-control" id="suite_civic" placeholder="House number" name="civic">
+            <input type="text" class="form-control" id="suite_civic" placeholder="House number" name="civic" value="{{ old('civic') }}" required >
         </div>
         @error('civic')
             <span class="bg-danger" role="alert">
@@ -74,7 +74,7 @@
         
         <div class="w-50 m-5">
             <label for="suite_city" class="form-label">City:</label>
-            <input type="text" class="form-control" id="suite_city" placeholder="City" name="city">
+            <input type="text" class="form-control" id="suite_city" placeholder="City" name="city" value="{{ old('city')}}" required >
         </div>
         @error('city')
             <span class="bg-danger" role="alert">
@@ -83,23 +83,27 @@
         @enderror
         <div class="w-50 m-5">
             <label for="suite_cap" class="form-label">CAP:</label>
-            <input type="text" class="form-control" id="suite_cap" placeholder="CAP" name="cap">
+            <input type="number" class="form-control" id="suite_cap" placeholder="CAP" name="cap" value="{{ old('cap')}}" required >
         </div>
         @error('cap')
             <span class="bg-danger" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
-        @enderror
+        @enderror --}}
         <div class="w-50 m-5">
-            <label for="suite_img" class="form-label">Upload Suite Images:</label>
+            <label for="suite_img" class="form-label">*Upload Suite Images:</label>
           
-            <input type="file" class="form-control" name="img" id="Suite_img" placeholder=""/>
+            <input type="file" class="form-control" name="img" id="Suite_img" value="{{ old('img')}}" accept=".png, .jpg, .jpeg" required />
         </div>
         @error('img')
             <span class="bg-danger" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
         @enderror
+
+        <div class="text-end">
+            <p class="my-fs">* campo obbligatorio</p>
+        </div>
         
 
        
@@ -132,4 +136,12 @@
 
         <button  type="submit" class="btn btn-primary fs-5 mx-5 mb-5"> Add Suite </button>
     </form>
+
+    <style scoped>
+        .my-fs{
+            font-size: 10px
+        }
+    </style>
+
+
 @endsection
