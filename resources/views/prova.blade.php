7e1f5c7420b4f97868f2d3309630d785bfbcb6fd
AntonioDattolo <antonio19794.a@gmail.com>
2024-09-04 14:44:51 +0200

    fix something

--- resources/views/prova.blade.php
+++ resources/views/prova.blade.php
@@ -4,6 +4,6 @@
 <div class="jumbotron p-5 mb-4 bg-light rounded-3">
    ciaoooo
  
-   {{$apartment}}
+   alalalalahagahhaa
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,13 @@
         {{-- <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p> --}}
         {{-- <a href="{{ route('register') }}" class="btn btn-primary btn-lg" type="button">{{ __('Register') }}</a> --}}
         @guest
+        <a href="{{ route('register') }}" class="btn btn-primary btn-lg" type="button">{{ __('Register') }}</a>
         @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="btn btn-primary btn-lg" type="button">{{ __('Register') }}</a>
+            {{-- searchbar --}}
+            <input id="searchBar" type="text">
+            {{-- container risultati --}}
+            <div id="risultatiRicerca"></div>
+            
         @endif
 
         @else
@@ -23,7 +28,9 @@
         <a href="{{ route('admin.suite.create') }}" class="btn btn-warning btn-lg" type="button">Add a Suite</a>
         <br>
         <a href="{{ route('admin.suite.index') }}" class="btn btn-info btn-lg mt-4" type="button">My Suites</a>
-        
+        sono un admin 
+         
+
 
         @endguest
         <br>
@@ -36,4 +43,31 @@
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
     </div> --}}
 </div>
+<script>
+    const searchBar = document.getElementById("searchBar")
+    searchBar.addEventListener("input", getInputSearch);
+    let search_input = 'ipsum'
+    function getInputSearch(value){
+         search_input = value.target.value
+         getSearch();
+         return search_input.toLowerCase()
+    }
+
+    function getSearch(){
+       console.log(search_input)
+       let y = "via giovanni verdi, 25, Roma"
+       if(y.toLowerCase().includes(search_input.toLowerCase())){
+         return console.log("si cazzo")
+       }else{
+        return console.log('non ci hai capito una minchia')
+       }
+
+           
+
+    }
+    
+
+
+
+</script>
 @endsection
