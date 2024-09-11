ba48057ba305aaaf45b82c8cab1dd690d3bb1e50
francescofalcone24 <cicciofalcon97@gmail.com>
2024-09-11 13:10:52 +0300

    messages fixed

--- resources/views/admin/suite/show.blade.php
+++ resources/views/admin/suite/show.blade.php
@@ -9,180 +9,115 @@
 		</div>
 	@endif
 	<h1 class="card-title ms-4 mt-5">{{ $selectedSuite->title }}</h1>
-	<div class="jumbotron p-3 mb-4 rounded-3 d-flex">
-		<div class="col-7">
-			@if (Str::startsWith($selectedSuite->img, 'http'))
-				<img class="card-img-top object-fit-contain rounded p-2" src="{{ $selectedSuite['img'] }}">
-			@else
-				<img class="card-img-top object-fit-contain rounded p-2" src="{{ asset('/storage/' . $selectedSuite->img) }}">
-			@endif
-		</div>
-		<div class="card-body col-5 px-5">
-
-			<h3 class="mb-5">Suite informations:</h3>
-			<h4>
-				<p>
-					Rooms: {{ $selectedSuite->room }}
-				</p>
-				<p>
-					Bathrooms: {{ $selectedSuite->bathroom }}
-				</p>
-				<p>
-					Beds: {{ $selectedSuite->bed }}
-				</p>
-
-				{{-- Indirizzo: --}}
-				{{-- @foreach ($address as $key => $item)
-                  @if ($key <= 1)
-                    <span>
-                      {{$item}}  
-                    </span>
-                    @endif
-                    @if ($key == 2)
-                    <br>
-                    <span class="">
-                      {{$item}} 
-                    </span>
-                    @endif
-                    @if ($key == 3)
-                    <span>{{$item}} </span> 
-                    @endif
-                @endforeach --}}
-				<p>
-					Address: {{ $selectedSuite->address }}
-				</p>
-
-				<a href=" {{ route('admin.suite.edit', $selectedSuite->id) }} " class="btn btn-primary my-2">EDIT</a>
-				<a href="http://localhost:8000/admin/payment" class="btn btn-success">Sponsorizza <i
-						class="fa-solid fa-coins text-warning"></i></a>
-				@if ($selectedSuite->messages != [])
-					<div class="d-flex flex-wrap">
-						@foreach ($selectedSuite->messages as $message)
-							<div class="m-2 bg-warning p-4 border rounded">
-
-								<p class="fs-6">
-									Da:{{ $message->name }}
-								</p>
-								<p class="fs-6">
-									Email:{{ $message->email }}
-								</p>
-								<h5>{{ $message->text }}</h5>
-							</div>
-					</div>
-				@endforeach
+	<div class="jumbotron p-3 mb-4 rounded-3 ">
+		<div class="row">
+			<div class="col-6">
+				@if (Str::startsWith($selectedSuite->img, 'http'))
+					<img class="card-img-top object-fit-contain rounded p-2" src="{{ $selectedSuite['img'] }}">
+				@else
+					<img class="card-img-top object-fit-contain rounded p-2" src="{{ asset('/storage/' . $selectedSuite->img) }}">
 				@endif
-				{{-- __________________________________________________________________ --}}
-				<button type="button" class="btn btn-danger" data-bs-toggle="modal"
-					data-bs-target="#modal-{{ $selectedSuite->id }}">
-					DELETE
-				</button>
-				{{-- <h5>Latitudine: {{$selectedSuite->latitude}}</h5>
-	@if (session('message'))
-		<div class="alert alert-success alert-dismissible fade show mt-2" role="alert">
-			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-			<strong>{{ session('message') }}</strong>
-		</div>
-	@endif
-	<h1 class="card-title ms-4 mt-5">{{ $selectedSuite->title }}</h1>
-	<div class="jumbotron p-3 mb-4 rounded-3 d-flex">
-		<div class="col-7">
-			@if (Str::startsWith($selectedSuite->img, 'http'))
-				<img class="card-img-top object-fit-contain rounded p-2" src="{{ $selectedSuite['img'] }}">
-			@else
-				<img class="card-img-top object-fit-contain rounded p-2" src="{{ asset('/storage/' . $selectedSuite->img) }}">
-			@endif
-		</div>
-		<div class="card-body col-5 px-5">
+			</div>
+			<div class="card-body col-6 px-5">
 
-			<h3 class="mb-5">Suite informations:</h3>
-			<h4>
-				<p>
-					Rooms: {{ $selectedSuite->room }}
-				</p>
-				<p>
-					Bathrooms: {{ $selectedSuite->bathroom }}
-				</p>
-				<p>
-					Beds: {{ $selectedSuite->bed }}
-				</p>
-				<p>
-					Address: {{ $selectedSuite->address }}
-				</p>
+				<h3 class="mb-5">Suite informations:</h3>
+				<h4>
+					<p>
+						Rooms: {{ $selectedSuite->room }}
+					</p>
+					<p>
+						Bathrooms: {{ $selectedSuite->bathroom }}
+					</p>
+					<p>
+						Beds: {{ $selectedSuite->bed }}
+					</p>
+					<p>
+						Address: {{ $selectedSuite->address }}
+					</p>
 
-				<a href=" {{ route('admin.suite.edit', $selectedSuite->id) }} " class="btn btn-primary my-2">EDIT</a>
-				{{-- __________________________________________________________________ --}}
-				<button type="button" class="btn btn-danger" data-bs-toggle="modal"
-					data-bs-target="#modal-{{ $selectedSuite->id }}">
-					DELETE
-				</button>
-				@if ($selectedSuite->messages != [])
-					<div class="d-flex flex-wrap">
-						@foreach ($selectedSuite->messages as $message)
-							<div class="m-2 bg-warning p-4 border rounded">
+					<a href=" {{ route('admin.suite.edit', $selectedSuite->id) }} " class="btn btn-primary my-2">EDIT</a>
+					<a href="http://localhost:8000/admin/payment" class="btn btn-success">Sponsorizza <i
+							class="fa-solid fa-coins text-warning"></i></a>
 
-								<p class="fs-6">
-									Da:{{ $message->name }}
-								</p>
-								<p class="fs-6">
-									Email:{{ $message->email }}
-								</p>
-								<h5>{{ $message->text }}</h5>
-							</div>
-					</div>
-				@endforeach
-				@endif
-				{{-- <h5>Latitudine: {{$selectedSuite->latitude}}</h5>
-            <h5>Longitudine: {{$selectedSuite->longitude}}</h5> --}}
+					{{-- __________________________________________________________________ --}}
+					<button type="button" class="btn btn-danger" data-bs-toggle="modal"
+						data-bs-target="#modal-{{ $selectedSuite->id }}">
+						DELETE
+					</button>
 
-				<div class="modal fade" id="modal-{{ $selectedSuite->id }}" tabindex="-1" data-bs-backdrop="static"
-					data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{ $selectedSuite->id }}" aria-hidden="true">
-					<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm " role="document">
-						<div class="modal-content bg-dark">
-							<div class="modal-header">
-								<h5 class="modal-title text-white" id="modalTitle-{{ $selectedSuite->id }}">
-									Delete suite
-								</h5>
-								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-							</div>
+					{{-- INIZIO SEZIONE MESSAGGI --}}
+					@if ($selectedSuite->messages != [])
+						<div class="">
+							@foreach ($selectedSuite->messages as $message)
+								<div class="my-2 bg-warning p-2 border rounded">
+									@if ($message->name != null)
+										<span class="fs-6">
+											Da:{{ $message->name }}
+										</span>
+									@endif
+									<span class="fs-6">
+										Email:{{ $message->email }}
+									</span>
+									<p>{{ $message->text }}</p>
+								</div>
+							@endforeach
+						</div>
+					@endif
+					{{-- FINE SEZIONE MESSAGGI --}}
 
-							<div class="modal-body text-center">
-								<span class="text-white">
-									Are you sure you want to delete
+					{{-- INIZIO SEZIONE MODALE --}}
+
+					<div class="modal fade" id="modal-{{ $selectedSuite->id }}" tabindex="-1" data-bs-backdrop="static"
+						data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{ $selectedSuite->id }}" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm " role="document">
+							<div class="modal-content bg-dark">
+								<div class="modal-header">
+									<h5 class="modal-title text-white" id="modalTitle-{{ $selectedSuite->id }}">
+										Delete suite
+									</h5>
+									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+								</div>
+
+								<div class="modal-body text-center">
+									<span class="text-white">
+										Are you sure you want to delete
+										<br>
+										<strong>
+											{{ $selectedSuite->title }}?
+										</strong>
+									</span>
 									<br>
-									<strong>
-										{{ $selectedSuite->title }}?
-									</strong>
-								</span>
-								<br>
-								<span class="text-danger">
-									Danger, you cannot undo this operation
-								</span>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-									Close
-								</button>
-
-								<form action="{{ route('admin.suite.destroy', $selectedSuite->id) }}" method="post">
-									@csrf
-									@method('DELETE')
-
-									<button type="submit" class="btn btn-danger">
-										Confirm
+									<span class="text-danger">
+										Danger, you cannot undo this operation
+									</span>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+										Close
 									</button>
 
-								</form>
+									<form action="{{ route('admin.suite.destroy', $selectedSuite->id) }}" method="post">
+										@csrf
+										@method('DELETE')
+
+										<button type="submit" class="btn btn-danger">
+											Confirm
+										</button>
+
+									</form>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+					{{-- FINE SEZIONE MODALE --}}
 
 
 
 
 
 
-				{{-- <form action="{{ route('admin.suite.destroy', $selectedSuite->id) }}" method="post">
+
+					{{-- <form action="{{ route('admin.suite.destroy', $selectedSuite->id) }}" method="post">
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="btn btn-danger ">
@@ -191,6 +126,7 @@
                 </form> --}}
 
 
+			</div>
 		</div>
 	</div>
 @endsection
