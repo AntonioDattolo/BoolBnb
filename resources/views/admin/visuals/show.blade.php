6a548f822e0ed74175df295749c05264bd80f1af
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-09-13 00:59:50 +0200

    blade push

---
+++ resources/views/admin/visuals/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('content')
+<div>
+    <canvas id="myChart"></canvas>
+  </div>
+  
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+ <?php 
+ $months = $visuals->pluck('month')->all();
+ $counts = $visuals->pluck('visuals')->all();
+ ?>
+  <script>
+    const ctx = document.getElementById('myChart');
+  
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: <?php  echo json_encode($months); ?>,
+
+        // ['january', 'febrary', 'march', 'april', 'may', 'june',  'july',  'august', 'september', 'october', 'november', 'december'],
+        
+        datasets: [{
+          label: 'Visuals',
+          data: <?php   echo json_encode($counts); ?>,
+
+          borderWidth: 2,
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  </script>
+  
+
+@endsection
