8945e33427ac4db791cb63310caa69473ed826cc
AntonioDattolo <antonio19794.a@gmail.com>
2024-09-12 17:04:11 +0200

    fix

--- app/Http/Controllers/Api/VisualController.php
+++ app/Http/Controllers/Api/VisualController.php
@@ -24,34 +24,38 @@ class VisualController extends Controller
     public function store(Request $request) {
         // 
         // $suite = Suite::where('slug', $slug)->first();
-        $data = $request->all();
-        $data['date'] = now();
+         $data = $request->all();
+         $data['date'] = now();
 
-        $validator = FacadesValidator::make($data, [
-            'ip_address' => 'required',
-            'suite_id' => 'required',
-            // 'date' => 'nullable', 
-        ]);
-        if ($validator->fails()) {
-            return response()->json([
-                'success' => false,
-                'errors' => $validator->errors(),
-            ]);
-        }
+        // $validator = FacadesValidator::make($data, [
+        //     'ip_address' => 'required',
+        //     'suite_id' => 'required',
+        //     // 'date' => 'nullable', 
+        // ]);
+        // if ($validator->fails()) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'errors' => $validator->errors(),
+        //     ]);
+        // }
         
-        $new_visual = new Visual();
-        $new_visual->fill($data);
+         $new_visual = new Visual();
+        // $new_visual->fill($data);
+        $new_visual->suite_id = $data['suite'];
+        $new_visual->ip_address = $data['ip'];
+        $new_visual->date = $data['date'];
+        // dd($new_visual);
         $new_visual->save();
         // $new_visual = Visual::create([
-        //     'ip_address' => $data->visuals->ip_address,
-        //     'suite_id' => $data->visuals->suite_id,
-        //     'date' => now(),
-        // ]) ;
-        // $new_visual->save();
+        //      'ip_address' => $data->visuals->ip_address,
+        //      'suite_id' => $data->visuals->suite_id,
+        //      'date' => now(),
+        //  ]) ;
+        //  $new_visual->save();
 
         return response()->json([
             'success' => true,
-            'results' => $data, 'bella', 
+            'results' => $request->all()
         ]);
     
        
