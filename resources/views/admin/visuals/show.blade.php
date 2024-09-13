ca81dca4424357fa51cfb4e7bb896b2669855716
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-09-13 17:24:52 +0200

    grafico aggiornato

--- resources/views/admin/visuals/show.blade.php
+++ resources/views/admin/visuals/show.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.admin')
 
 @section('content')
-<div>
+<div class="container w-50 p-3">
+  <h3 class="text-center">Your visuals statistics analizer</h3>
     <canvas id="myChart"></canvas>
   </div>
   
@@ -9,30 +10,46 @@
  <?php 
  $months = $visuals->pluck('month')->all();
  $counts = $visuals->pluck('visuals')->all();
+ $currentMonth = date('n');
+ $monthDue = [];
+ $monthNames = [
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+];
+// dd($currentMonth);
+// array_push(array, value1, value2, ..., valueN)
+$monthsInLetters = array_map(function($months) use ($monthNames) {
+  return $monthNames[$months];}, $months);
+
  ?>
   <script>
     const ctx = document.getElementById('myChart');
-  
+    Chart.defaults.font.size = 20;
     new Chart(ctx, {
-      type: 'line',
+      type: 'bar',
       data: {
-        labels: <?php  echo json_encode($months); ?>,
-
-        // ['january', 'febrary', 'march', 'april', 'may', 'june',  'july',  'august', 'september', 'october', 'november', 'december'],
-        
+        labels: <?php  echo json_encode($monthsInLetters);  ?>,
+        // 
         datasets: [{
-          label: 'Visuals',
+          label: 'Your Visuals',
           data: <?php   echo json_encode($counts); ?>,
 
           borderWidth: 2,
         }]
       },
       options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
+        // animations: { tension: { duration: 1000, easing: 'linear', from: 1, to: 0, loop: true }},
+        scales: { y: { beginAtZero: true, }, x: {} },
       }
     });
   </script>
