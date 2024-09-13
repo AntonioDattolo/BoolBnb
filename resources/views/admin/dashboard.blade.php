0166624dc4b4222c54f5f564ea227212cefdfb30
francescofalcone24 <cicciofalcon97@gmail.com>
2024-09-13 19:20:02 +0300

    style dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,38 +1,46 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+	<section class="p-0">
+		<div class="container-fluid mt-4 h-100">
+			<div class="row justify-content-center align-items-center h-100">
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-header text-center">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+						<div class="card-body">
+							@if (session('status'))
+								<div class="alert alert-success" role="alert">
+									{{ session('status') }}
+								</div>
+							@endif
 
-                        <p class="fs-4 fw-semibold">{{ __('Welcome')  }} {{ Auth::user()->name}}</p>
-                        
-                        {{-- ricavare id per apartments --}}
-                        {{-- {{ Auth::user()->id }} --}}
-                        {{-- <a href="{{route('admin.suite.index') }}">miei appartamenti</a> --}}
-                        <a href="{{ route('admin.suite.create') }}" class="btn btn-warning btn-lg" type="button">Add a Suite</a>
-                        <br>
-                        <a href="{{ route('admin.suite.index') }}" class="btn btn-info btn-lg mt-4" type="button">My Suites</a>
-                        {{-- route('admin.suite')  --}}
-                        {{-- ------------- --}}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+							<p class="fs-4 fw-semibold text-center">{{ __('Welcome') }} {{ Auth::user()->name }}</p>
+
+							<div class="text-center">
+								<a href="{{ route('admin.suite.create') }}" class="btn btn-warning btn-lg" type="button">Add a Suite</a>
+
+								<a href="{{ route('admin.suite.index') }}" class="btn btn-info btn-lg " type="button">My Suites</a>
+							</div>
+
+
+							{{-- ------------- --}}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 @endsection
 
 <style scoped>
-    .card{
-        z-index: 1;
-    }
+	.card {
+		z-index: 1;
+	}
+
+	section {
+		background-image: url('/img/interno.jpg');
+		background-size: contain;
+		height: 90%;
+	}
 </style>
