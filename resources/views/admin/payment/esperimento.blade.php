916ca8193b73f8486b784f85bddbd88e93c7aa36
AntonioDattolo <antonio19794.a@gmail.com>
2024-09-17 00:56:15 +0200

    fix v5

--- resources/views/admin/payment/esperimento.blade.php
+++ resources/views/admin/payment/esperimento.blade.php
@@ -1,92 +1,99 @@
 @extends('layouts.admin')
 
 @section('content')
-<section class="container d-flex justify-content-center flex-wrap">
+    <section class="container d-flex justify-content-center flex-wrap">
 
 
-    <form id="sponsorships_update" action="{{ route('admin.suite.update', $suite[0]->id)}}" method="POST"
-        enctype="multipart/form-data">
-        @method('PUT')
-        @csrf
-        <section class="container pt-5 d-flex justify-content-center flex-wrap">
+        <form id="sponsorships_update" action="{{ route('admin.suite.update', $suite[0]->id) }}" method="POST"
+            enctype="multipart/form-data">
+            @method('PUT')
+            @csrf
+            <section class="container pt-5 d-flex justify-content-center flex-wrap">
 
-            <div class="mb-4 row col-12">
-                <h2 class="col-12 mb-3 col-form-label text-md-right">You are sponsoring :</h2>
-                <h1 class="col-12 mb-3">{{ $suite[0]->title }}</h1>
-                <h4 class="col-12 mb-5">Situated in : {{ $suite[0]->address }}</h4>
-                <div class="col-12 d-flex gap-3 justify-content-center flex-wrap">
-                    @foreach ($sponsor as $item)
-                        <div class="card col-12 col-md-3">
-                            
-                            <div class="card-body" style="position: relative">
-                                <div style="height: 2rem"></div>
-                                <div class="form-check text-center">
-                                    <input class="form-check-input" type="radio" name="sponsorship" id="sponsor"
-                                        value="{{ $item->id }}" style="border: 1.5px solid rgb(48, 48, 48)">
-                                    <label class="form-check-label" for="sponsor">
-                                        <h5 class="card-title">{{ $item->name }}</h5>
-                                    </label>
-                                </div>
+                <div class="mb-4 row col-12">
+                    <h2 class="col-12 mb-3 col-form-label text-md-right text-white">You are sponsoring :</h2>
+                    <h1 class="col-12 mb-3 text-white" >{{ $suite[0]->title }}</h1>
+                    <h4 class="col-12 mb-5 text-white">Situated in : {{ $suite[0]->address }}</h4>
+                    <div class="col-12 d-flex gap-3 justify-content-center flex-wrap">
+                        @foreach ($sponsor as $item)
+                            <div class="card col-12 col-md-3 sponsor_card">
 
-                                <h6 class="card-subtitle mb-2 text-body-secondary"> {{ $item->price }} $ </h6>
-                                <p>FOR</p>
-                                <p class="card-text text-center">{{ str_replace(':00:00', '', $item->period) }} Hours in featured </p>
-                                    @if($item->id == 2)
-                                    <span class="" style="width:100%px;position: absolute;top: 0px; right:0;padding: 0 3px 0 5px;border-radius:5px; border-top: 35px solid rgb(42, 66, 252);border-left: 29px solid rgba(77, 41, 223, 0); color:whitesmoke">
-                                        <span style="display :block;position:relative;bottom: 22px ">Best seller</span>
-                                      
-                                    </span>
-                                    @endif
+                                <label class="form-check-label" for="sponsor{{ $item->id }}" style="cursor: pointer" >
+                                    <div class="card-body" style="position: relative">
+                                        <div style="height: 2rem"></div>
+                                        <div class="form-check text-start">
+                                            <h5 class="card-title">
+                                            <input type="radio" name="sponsorship" id="sponsor{{ $item->id }}"
+                                                value="{{ $item->id }}" style="border: 1.5px solid rgb(48, 48, 48)">
+                                                {{ $item->name }}
+                                            </h5>
+                                        </div>
+
+                                        <h6 class="card-subtitle mb-2 text-body-secondary"> {{ $item->price }} $ </h6>
+                                        <p>FOR</p>
+                                        <p class="card-text text-center">{{ str_replace(':00:00', '', $item->period) }}
+                                            Hours in featured </p>
+                                        @if ($item->id == 2)
+                                            <span class=""
+                                                style="width:100%px;position: absolute;top: 0px; right:0;padding: 0 3px 0 5px;border-radius:5px; border-top: 35px solid rgb(42, 66, 252);border-left: 29px solid rgba(77, 41, 223, 0); color:whitesmoke">
+                                                <span style="display :block;position:relative;bottom: 22px ">Best
+                                                    seller</span>
+
+                                            </span>
+                                        @endif
+                                    </div>
+                                </label>
                             </div>
-                        </div>
-                    @endforeach
-                    {{-- @error('spo')
+                        @endforeach
+                        {{-- @error('spo')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror --}}
-                </div>
-            </div>
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </section>  {{-- <button type="submit">premi</button> --}}
-        
-    </form>
-
-    <div class="col-12 d-flex justify-content-center">
-        <div class="col-12 d-flex justify-content-center">
-
-            <form class="d-flex justify-content-center flex-wrap" method="post" id="payment-form" action="{{ url('/checkout') }}">
-                @csrf
-                <section class="w-100">
-                    <div class="bt-drop-in-wrapper">
-                        <div id="bt-dropin"></div>
                     </div>
-                </section>
+                </div>
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
 
-                <input id="nonce" name="payment_method_nonce" type="hidden" />
-                <button id="pay" class="btn btn-primary" type="submit"><span>Take your BOOST &#128640; </span></button>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+            </section> {{-- <button type="submit">premi</button> --}}
 
-            </form>
-            <div id="container_button"></div>
+        </form>
 
+        <div class="col-12 d-flex justify-content-center">
+            <div class="col-12 d-flex justify-content-center">
+
+                <form class="d-flex justify-content-center flex-wrap" method="post" id="payment-form"
+                    action="{{ url('/checkout') }}">
+                    @csrf
+                    <section class="w-100">
+                        <div class="bt-drop-in-wrapper">
+                            <div id="bt-dropin"></div>
+                        </div>
+                    </section>
+
+                    <input id="nonce" name="payment_method_nonce" type="hidden" />
+                    <button id="pay" class="btn btn-primary" type="submit"><span>Take your BOOST &#128640;
+                        </span></button>
+
+                </form>
+                <div id="container_button"></div>
+
+            </div>
         </div>
-    </div>
-    </div>
+        </div>
 
-</section>
-    <script src="https://js.braintreegateway.com/web/dropin/1.13.0/js/dropin.min.js"></script>
+    </section>
+    <script src="https://js.braintreegateway.com/web/dropin/1.43.0/js/dropin.min.js"></script>
     <script>
         var form = document.querySelector('#payment-form');
         var client_token = "{{ $token }}";
@@ -109,11 +116,14 @@
                 authorization: client_token,
                 selector: '#bt-dropin',
                 
+                
+
             }, function(createErr, instance) {
                 if (createErr) {
                     console.log('Create Error', createErr);
                     return;
                 }
+                
                 form.addEventListener('click', function(event) {
                     event.preventDefault();
 
@@ -150,7 +160,14 @@
             height: 100vh;
             margin: 0;
         }
+        .sponsor_card:hover{
+            transform: scale(1.05);
+            background-color:  rgba(255, 162, 22, 0.795);
+            color: whitesmoke;
+            border-radius: 5px;
+            box-shadow: 0 0 5px rgb(23, 23, 23);;
 
+        }
         .full-height {
             height: 100vh;
         }
